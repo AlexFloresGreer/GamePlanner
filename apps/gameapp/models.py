02902e2f70a260edc7ca9913e9aa54b549e426ea
lynamemi <lynam.emily@gmail.com>
2016-08-24 10:24:24 -0700

    css for loginreg, added admin, added model

--- apps/gameapp/models.py
+++ apps/gameapp/models.py
@@ -6,3 +6,14 @@ import bcrypt
 from datetime import datetime, date, timedelta, time
 from time import strftime
 # Create your models here.
+
+class Game(models.Model):
+	game = models.CharField(max_length=100)
+	location = models.CharField(max_length=100)
+	players = models.CharField(max_length=100)
+	date = models.DateField()
+	time = models.TimeField()
+	game_starter = models.ForeignKey('loginreg.User', related_name="usersstarts")
+	game_joiner = models.ManyToManyField('loginreg.User', related_name="jointrip")
+	created_at = models.DateTimeField(auto_now_add = True)
+	updated_at = models.DateTimeField(auto_now = True)
